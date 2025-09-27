003451e4c0cdebef530312851f412f0c7ea96445
GMonchain <gmonchain@gmail.com>
2025-09-27 16:54:02 +0700

    feat: Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -154,7 +154,7 @@ export default function HomePage() {
         onDownload={handleDownload}
       />
 
-      {/* Toast Notifications */}
+      {/* Toast Notifications for user feedback */}
       <ToastContainer toasts={toasts} onClose={removeToast} />
     </div>
   );
