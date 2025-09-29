c7641d8039232a9198455872a00fd1540f7b6278
GMonchain <gmonchain@gmail.com>
2025-09-29 17:04:08 +0700

    feat: Document handlePreview function

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,7 +43,7 @@ export default function HomePage() {
     addToast(`${icon.displayName} downloaded!`, 'success');
   };
 
-  const handlePreview = (icon: CryptoIcon) => {
+  const handlePreview = (icon: CryptoIcon) => { // Handles opening the preview modal for an icon
     setSelectedIcon(icon);
     setIsModalOpen(true);
   };
