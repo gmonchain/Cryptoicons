1903eaa8edbca89fb3cd5fc14fd67d4af24aff03
GMonchain <gmonchain@gmail.com>
2025-09-29 16:58:01 +0700

    feat: Add comment for IconCard component

--- pages/index.tsx
+++ pages/index.tsx
@@ -126,6 +126,7 @@ export default function HomePage() {
         {filteredIcons.length > 0 ? (
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-6">
             {filteredIcons.map((icon) => (
+              {/* Individual icon card component */}
               <IconCard
                 key={icon.name}
                 icon={icon}
