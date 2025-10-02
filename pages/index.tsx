4c300e971c68e6a550e7607e42eb85e22ef080c0
GMonchain <gmonchain@gmail.com>
2025-10-02 12:51:27 +0700

    Update pages/index.tsx - Modifications 17 and 18

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const HomePage: React.FC = () => {
   return (
     <div>
-      <h1>Welcome to Cryptoicons! Modification 15</h1>
-      <p>This is the initial content of the home page. Modification 16</p>
+      <h1>Welcome to Cryptoicons! Modification 17</h1>
+      <p>This is the initial content of the home page. Modification 18</p>
     </div>
   );
 };
