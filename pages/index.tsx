f93b20903f30fd976619570985e67c7d8f814d58
GMonchain <gmonchain@gmail.com>
2025-10-03 16:52:28 +0700

    Added comment for ToastContainer import

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { SearchBar } from '../components/SearchBar'; // Component for search inp
 import { Stats } from '../components/Stats'; // Component to display icon statistics
 import { IconCard } from '../components/IconCard'; // Component for individual icon display
 import { PreviewModal } from '../components/PreviewModal'; // Modal for displaying detailed icon preview
-import { ToastContainer } from '../components/Toast';
+import { ToastContainer } from '../components/Toast'; // Container for displaying toast notifications
 import { useCryptoIcons } from '../hooks/useCryptoIcons';
 import { useToast } from '../hooks/useToast';
 import { CryptoIcon } from '../types';
