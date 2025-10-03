2f86bba2a018c40266226582eb61e91978dd5bf4
GMonchain <gmonchain@gmail.com>
2025-10-03 14:19:41 +0700

    Feat: Add comment for initiating download

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,7 +39,7 @@ export default function HomePage() {
     link.download = icon.fileName; // Sets the suggested filename for the downloaded file
     document.body.appendChild(link); // Appends the link to the document body (necessary for Firefox)
     link.click(); // Programmatically clicks the link to initiate download
-    document.body.removeChild(link);
+    document.body.removeChild(link); // Removes the temporary link from the document body
     addToast(`${icon.displayName} downloaded!`, 'success');
   };
 
