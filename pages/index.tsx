88f697f443b359baac4705323aad257639b2d44e
GMonchain <gmonchain@gmail.com>
2025-10-03 15:40:42 +0700

    Refactor: Remove SearchBar component comment

--- pages/index.tsx
+++ pages/index.tsx
@@ -83,7 +83,7 @@ export default function HomePage() {
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         {/* Search Bar */}
         <div className="max-w-2xl mx-auto mb-8">
-          <SearchBar // Component for searching icons
+          <SearchBar
             value={searchQuery}
             onChange={setSearchQuery}
             placeholder="Search crypto icons by name or symbol..."
