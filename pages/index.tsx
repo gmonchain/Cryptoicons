0d5263a93b942321eac4e958ca7a899c862e75b2
GMonchain <gmonchain@gmail.com>
2025-10-03 14:29:18 +0700

    Refactor: Add comment for IconCard component props

--- pages/index.tsx
+++ pages/index.tsx
@@ -118,6 +118,7 @@ export default function HomePage() {
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-6"> {/* Responsive grid for displaying icon cards */}
             {filteredIcons.map((icon) => (
               <IconCard // Individual icon card component
+                {/* IconCard component props */}
                 key={icon.name}
                 icon={icon}
                 onCopy={handleCopy}
