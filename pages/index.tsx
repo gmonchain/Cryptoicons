43f04fce4736c4d461ae5c0559e1bf408a35cb81
GMonchain <gmonchain@gmail.com>
2025-10-03 14:31:00 +0700

    Refactor: Add comment for ToastContainer import

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { SearchBar } from '../components/SearchBar'; // Component for searching 
 import { Stats } from '../components/Stats'; // Component for displaying statistics
 import { IconCard } from '../components/IconCard'; // Component for individual icon display
 import { PreviewModal } from '../components/PreviewModal'; // Component for showing a larger icon preview
-import { ToastContainer } from '../components/Toast';
+import { ToastContainer } from '../components/Toast'; // Component for displaying toast notifications
 import { useCryptoIcons } from '../hooks/useCryptoIcons';
 import { useToast } from '../hooks/useToast';
 import { CryptoIcon } from '../types';
