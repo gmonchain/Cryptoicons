710ba21cecfb3857445dd7055ff4d196cf0e8768
GMonchain <gmonchain@gmail.com>
2025-10-03 15:18:08 +0700

    Feat: Add comment to Stats totalIcons prop

--- pages/index.tsx
+++ pages/index.tsx
@@ -93,7 +93,7 @@ export default function HomePage() {
 
         {/* Stats */}
         <Stats // Component for displaying icon statistics
-          totalIcons={icons.length}
+          totalIcons={icons.length} // Total number of available icons
           filteredIcons={filteredIcons.length}
           isFiltered={!!searchQuery.trim()}
         />
