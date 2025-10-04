29a552754b542ebd52b6cf47f40f030a20ff4d80
GMonchain <gmonchain@gmail.com>
2025-10-04 12:03:24 +0700

    feat: Add comment for ToastContainer component

--- pages/index.tsx
+++ pages/index.tsx
@@ -144,7 +144,7 @@ export default function HomePage() {
       />
 
       {/* Toast Notifications */}
-      <ToastContainer toasts={toasts} onClose={removeToast} />
+      <ToastContainer toasts={toasts} onClose={removeToast} /> // Container for displaying toast notifications.
     </div>
   );
 }
