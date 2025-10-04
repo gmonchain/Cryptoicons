da86101b634a8110bc53fecc6cab38a4136b23e0
GMonchain <gmonchain@gmail.com>
2025-10-04 19:29:39 +0700

    feat: Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -134,7 +134,7 @@ export default function HomePage() {
       </main>
 
       {/* Preview Modal */}
-      <PreviewModal
+      <PreviewModal // Modal component for displaying a larger preview of the selected icon.
         icon={selectedIcon}
         isOpen={isModalOpen}
         onClose={handleCloseModal}
