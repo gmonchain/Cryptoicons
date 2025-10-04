d42c84da918e41ac2881490ab56653effe36ca03
GMonchain <gmonchain@gmail.com>
2025-10-04 19:29:48 +0700

    feat: Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -143,7 +143,7 @@ export default function HomePage() {
       />
 
       {/* Toast Notifications */}
-      <ToastContainer toasts={toasts} onClose={removeToast} />
+      <ToastContainer toasts={toasts} onClose={removeToast} /> // Container for displaying toast notifications.
     </div>
   );
 }
