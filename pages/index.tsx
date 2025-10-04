8f0613b0071b21bfb11c2ba1f9b1f5eb20cec139
GMonchain <gmonchain@gmail.com>
2025-10-04 19:30:08 +0700

    feat: Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useMemo } from 'react'; // React hooks for managing state and memoizing values.
-import { SearchBar } from '../components/SearchBar';
+import { SearchBar } from '../components/SearchBar'; // Component for searching icons.
 import { Stats } from '../components/Stats';
 import { IconCard } from '../components/IconCard';
 import { PreviewModal } from '../components/PreviewModal';
