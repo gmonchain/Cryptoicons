d4afc1bb4524149f7999309e0ddb0b1e4bc65fee
GMonchain <gmonchain@gmail.com>
2025-10-04 10:16:51 +0700

    feat: Add blank line between imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 // This is the main page of the Cryptoicons application
 import { useState, useMemo } from 'react';
 import { SearchBar } from '../components/SearchBar';
+
 import { Stats } from '../components/Stats';
 import { IconCard } from '../components/IconCard';
 import { PreviewModal } from '../components/PreviewModal';
